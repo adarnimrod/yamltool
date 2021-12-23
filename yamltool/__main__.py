821e7acb4c5c6f42484ca10d31b8562d1cf95302
Adar Nimrod <nimrod@shore.co.il>
2021-12-23 21:15:24 +0200

    Python module scaffolding.
    Just enough to build and install a wheel. Now all that's left is to
    write the code. Easy, right?

---
+++ yamltool/__main__.py
@@ -0,0 +1,15 @@
+"""YAML tool, a clone of the json.tool Python module for YAML.
+
+This tool provides a simple command line interface to validate and pretty-print
+YAML documents while trying to preserve as much as possible from the original
+documents (like comments and anchors).
+"""
+
+
+def main():
+    """Main entrypoint."""
+    return
+
+
+if __name__ == "__main__":
+    main()
